30d404f50b1e69a259aaa1b61a99d2cd6d2fb1a2
rodrigo-borges <rodrigo.sil.borges@gmail.com>
2019-05-21 10:32:05 -0300

    Remove test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-name = np.array(['ana','beto','caio','denis'])
-age = np.array([10,15,22,12])
-
-df = pd.DataFrame(data=dict(name=name, age=age))
-df.to_csv('test_df.csv', sep='\t')
